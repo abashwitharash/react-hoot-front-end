5761698381a5904ff47e41b4078098e94a05a472
Arash <arashsaleh798@gmail.com>
2025-03-18 09:38:00 -0700

    damn

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router';
-
+import * as hootService from './services/hootService';
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
-
+import HootList from './components/HootList/HootList';
 import { UserContext } from './contexts/UserContext';
+import HootDetails from './components/HootDetails/HootDetails';
 
 const App = () => {
+  const [hoots, setHoots] = useState([]);
   const { user } = useContext(UserContext);
-  
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+      // console log to verify
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots();
+  }, [user]);
   return (
     <>
       <NavBar/>
       <Routes>
         <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
+        {user ? (
+          <>
+            {/* Protected Routes (available only to signed-in users) */}
+            <Route path='/hoots' element={<HootList hoots={hoots}/>} />
+            {/* Add this route! */}
+            <Route 
+              path='/hoots/:hootId'
+              element={<HootDetails />}
+            />
+          </>
+        ) : (
+          <>
+            {/* Non-user Routes (available only to guests) */}
+            <Route path='/sign-up' element={<SignUpForm />} />
+            <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )}
       </Routes>
     </>
   );
 };
-
 export default App;
---
+++ src/components/HootDetails/HootDetails.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import * as hootService from '../../services/hootService';
+import { useParams } from 'react-router';
+
+
+const HootDetails = () => {
+    const [hoot, setHoot] = useState(null);
+
+    const { hootId } = useParams();
+    console.log('hootId', hootId);
+
+    useEffect(() => {
+        const fetchHoot = async () => {
+          const hootData = await hootService.show(hootId);
+          setHoot(hootData);
+        };
+        fetchHoot();
+      }, [hootId]);
+    
+      // Verify the hoot state is set correctly:
+      console.log('hoot state:', hoot);
+  
+    return <main>Hoot Details</main>;
+  };
+  
+  export default HootDetails;
